1e8938522534967f5dd8e9029081eda4dc643472
Kevin Quinália <kevin.quinalia@ifood.com.br>
2021-10-03 17:51:15 -0300

    add heatmap alerts

--- src/app/routes/heatmap.js
+++ src/app/routes/heatmap.js
@@ -17,8 +17,12 @@ Router.get('/location', async (req, res) => {
   if (!city) return res.status(400).send();
 
   const response = (await axios.get(`http://api.worldweatheronline.com/premium/v1/weather.ashx?key=${process.env.WWO_API_KEY}&q=${city}&num_of_days=2&format=json`)).data;
+  const feelsLikeC = response.data.current_condition[0].FeelsLikeC;
+
+  let avgYearlyTemp = 0;
 
   const customClimateAverage = response.data.ClimateAverages[0].month.map((month) => {
+    avgYearlyTemp = avgYearlyTemp + parseFloat(month.absMaxTemp);
     return {
       index: month.index,
       name: month.name,
@@ -27,13 +31,21 @@ Router.get('/location', async (req, res) => {
     }
   })
 
+  avgYearlyTemp = ((avgYearlyTemp) / 12);
+
+  // (> 30 && < 35) = 1, (> 35 && < 40) = 2, (> 40) = 3;
+  const yearlyTempAlert = avgYearlyTemp > 30 ? (avgYearlyTemp < 35 ? 1 : (avgYearlyTemp < 40 ? 2 : (avgYearlyTemp > 40 ? 3 : 0))) : 0;
+  const dailyFeelsLikeAlert = feelsLikeC > 30 ? (feelsLikeC < 35 ? 1 : (feelsLikeC < 40 ? 2 : (feelsLikeC > 40 ? 3 : 0))) : 0;
+
   const customData = {
     requestType: response.data.request[0].type,
     city: response.data.request[0].query,
     tempC: response.data.current_condition[0].temp_C,
-    feelsLikeC: response.data.current_condition[0].feelsLikeC,
+    feelsLikeC: feelsLikeC,
     weatherCode: response.data.current_condition[0].weatherCode,
-    climateAverage: customClimateAverage
+    climateAverage: customClimateAverage,
+    yearlyTempAlert: yearlyTempAlert,
+    dailyFeelsLikeAlert: dailyFeelsLikeAlert
   }
 
   return res.json({ ...customData })
